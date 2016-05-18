54d29743a1107b65e3bdbcb73c6945c6e64326cd
Tom Pittenger <magicrub@gmail.com>
2016-05-17 18:17:32 -0700

    AIRPHRAME: misc stuff

--- ArduPlane/Parameters.h
+++ ArduPlane/Parameters.h
@@ -344,6 +344,13 @@ public:
         k_param_mixing_offset,
         k_param_dspoiler_rud_rate,
 
+        // Airphrame specific
+        // ---------------
+        k_param_test1 = 250,
+        k_param_test2 = 251,
+        k_param_test3 = 252,
+        // ---------------
+
         k_param_DataFlash = 253, // Logging Group
 
         // 254,255: reserved
